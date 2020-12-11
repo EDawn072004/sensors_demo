38e79fb81feaf5c446f86c0b38f2c9ce4e97a8b6
EDawn072004 <69882381+EDawn072004@users.noreply.github.com>
2020-12-10 20:00:42 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-"""
-def on_received_string(receivedString):
-    acceleration = ""
-    if receivedString == "transmitter":
-        basic.show_string("R")
-        radio.send_value(acceleration, input.acceleration(Dimension.X))
-    if receivedString == acceleration:
-        serial.write_string("" + (acceleration))
-radio.on_received_string(on_received_string)
-"""
+def on_gesture_eight_g():
+    basic.show_leds("""
+        # # # # .
+        # . . . .
+        # # # . .
+        # . . . .
+        # . . . .
+        """)
+input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
+
+def on_button_pressed_a():
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# def on_received_string(receivedString):
+# acceleration = ""
+# if receivedString == "transmitter":
+# basic.show_string("R")
+# radio.send_value(acceleration, input.acceleration(Dimension.X))
+# if receivedString == acceleration:
+# serial.write_string("" + (acceleration))
+# radio.on_received_string(on_received_string)
 basic.show_string("T")
-input.set_accelerometer_range(AcceleratorRange.FOUR_G)
+input.set_accelerometer_range(AcceleratorRange.EIGHT_G)
 radio.set_group(204)
+
+def on_forever():
+    radio.send_number(input.acceleration(Dimension.Z))
+basic.forever(on_forever)
+
+degrees = 0
+
+def compass():
+    global degrees
+    degrees = input.compass_heading()
+    if degrees < 45:
+        basic.show_string("N")
+    elif degrees < 135:
+        basic.show_string("E")
+    elif degrees < 225:
+        basic.show_string("S")
+    elif degrees < 315:
+        basic.show_string("W")
+    else:
+        basic.show_string("N")
+basic.forever(compass)
--- main.ts
+++ main.ts
@@ -1,14 +1,44 @@
-// def on_received_string(receivedString):
-// acceleration = ""
-// if receivedString == "transmitter":
-// basic.show_string("R")
-// radio.send_value(acceleration, input.acceleration(Dimension.X))
-// if receivedString == acceleration:
-// serial.write_string("" + (acceleration))
-// radio.on_received_string(on_received_string)
-basic.showString("T")
-input.setAccelerometerRange(AcceleratorRange.FourG)
+input.onGesture(Gesture.EightG, function () {
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+let degrees = 0
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
+input.setAccelerometerRange(AcceleratorRange.EightG)
 radio.setGroup(204)
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.clearScreen()
+        led.toggle(2, 0)
+    } else if (degrees < 135) {
+        basic.clearScreen()
+        led.toggle(4, 2)
+    } else if (degrees < 225) {
+        basic.clearScreen()
+        led.toggle(2, 4)
+    } else if (degrees < 315) {
+        basic.clearScreen()
+        led.toggle(0, 2)
+    } else {
+        basic.clearScreen()
+        led.toggle(2, 0)
+    }
+})
 basic.forever(function () {
     radio.sendNumber(input.acceleration(Dimension.Z))
 })
